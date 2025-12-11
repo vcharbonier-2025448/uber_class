24482b16ea918fc6b22d2e63ba444e8068f6b5aa
Valeria Charbonier <2025448@student.cct.ie>
2025-12-11 12:40:42 +0000

    update4

--- uber.py
+++ uber.py
@@ -7,18 +7,30 @@ st.title("Uber Pickups in NYC")
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-date_column = "date/time"
+DATE_COLUMN = 'date/time'
+
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows= nrows)
-    lowrcase = lambda x: str(x).lower()
-    data.rename(lowrcase, axis="columns", inplace= True)
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
-    
-data_load_state = st.text("Data Loading...")
-data=load_data(10000)
-data_load_state.text("Done!")
 
-if st.checkbox("Show Raw Data"):
-    st.subheader("Row Data")
+data_load_state = st.text('Loading data...')
+data = load_data(100000)
+data_load_state.text('Done!')
+
+if st.checknox("Show Raw Data"):
+    st.subheader("Raw Data")
     st.write(data)
+
+st.subheader("Number of Pickups Per Hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour,
+                bin=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+hour_to_filter=st.slider("hour",0,23,17)
+filtered_data=data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader("Map of all pickups at %s:00" % hour_to_filter)
+st.map(filtered_data)
