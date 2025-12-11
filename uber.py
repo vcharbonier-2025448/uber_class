937b6c9c6b778fa5feafaa21cc5ebe0a72247427
Valeria Charbonier <2025448@student.cct.ie>
2025-12-11 12:22:09 +0000

    update

---
+++ uber.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Uber Pickups in NYC")
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+date_column = 'date/time'
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows= nrows)
+    lowrcase = lamba x: str(x).lower()
+    data.rename(lowrcase, axis='columns', inplace= True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
