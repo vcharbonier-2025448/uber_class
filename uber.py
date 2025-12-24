c1f12ab37a1ba8b5c3b3976313e395356ce700e9
Valeria Charbonier <2025448@student.cct.ie>
2025-12-24 02:05:31 +0000

    update 9

--- uber.py
+++ uber.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Energy Dashboard", layout="wide")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data.csv")
+    return pd.read_csv("ireland_energy.csv")
 
 df = load_data()
 
